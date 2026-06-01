b5b153ace8fb7b5b2c5a903438ab96a2adf88c3f
ZeeshanAmjad0495 <zeeshan04956@gmail.com>
2026-06-01 21:01:50 +0000

    feat(contracts): assertion primitives and branch progress

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -3,3 +3,4 @@ export type { Capability, CapabilityProbe } from "./capability";
 export type { StrategyKind, LocatorStrategy, Locator } from "./locator";
 export type { ElementHandle } from "./element";
 export type { GestureTarget, Action } from "./action";
+export type { ElementState, BranchProgress, Assertion } from "./assertion";
