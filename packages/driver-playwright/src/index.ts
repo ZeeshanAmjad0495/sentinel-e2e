e39e59fe756f08574f5aa65400ff0a5b14ae9e34
ZeeshanAmjad0495 <zeeshan04956@gmail.com>
2026-06-01 22:57:09 +0000

    feat(web): driver duck-types existingPage and creates sessions

--- packages/driver-playwright/src/index.ts
+++ packages/driver-playwright/src/index.ts
@@ -1,1 +1,8 @@
 export { PlaywrightDriver } from "./driver";
+export { PlaywrightSession } from "./session";
+export type { PlaywrightSessionOptions } from "./session";
+export { PlaywrightResolver } from "./resolver";
+export { PlaywrightAction } from "./action";
+export { PlaywrightAssertion } from "./assertion";
+export { PlaywrightElementHandle } from "./element";
+export { compileStrategy } from "./strategy-compiler";
